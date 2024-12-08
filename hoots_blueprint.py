98906ce4f3ba47251e24524307f9fc3c2ca17ef3
Ben Manning <bmanning@me.com>
2024-12-08 11:54:58 -0500

    hoot update functionality complete

--- hoots_blueprint.py
+++ hoots_blueprint.py
@@ -72,3 +72,27 @@ def show_hoot(hoot_id):
     except Exception as error:
         return jsonify({"error": str(error)}), 500
 
+
+@hoots_blueprint.route('/hoots/<hoot_id>', methods=['PUT'])
+@token_required
+def update_hoot(hoot_id):
+    try:
+        updated_hoot_data = request.json
+        connection = get_db_connection()
+        cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cursor.execute("SELECT * FROM hoots WHERE hoots.id = %s", (hoot_id,))
+        hoot_to_update = cursor.fetchone()
+        if hoot_to_update is None:
+            return jsonify({"error": "hoot not found"}), 404
+        connection.commit()
+        if hoot_to_update["author"] is not g.user["id"]:
+            return jsonify({"error": "Unauthorized"}), 401
+        cursor.execute("UPDATE hoots SET title = %s, text = %s, category = %s WHERE hoots.id = %s RETURNING *",
+                        (updated_hoot_data["title"], updated_hoot_data["text"], updated_hoot_data["category"], hoot_id))
+        updated_hoot = cursor.fetchone()
+        connection.commit()
+        connection.close()
+        return jsonify({"hoot": updated_hoot}), 200
+    except Exception as error:
+        return jsonify({"error": str(error)}), 500
+
