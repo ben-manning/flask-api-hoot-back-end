4dde6f6c83b1a58209629296f84da4840f9596fe
Ben Manning <bmanning@me.com>
2024-12-08 11:22:23 -0500

    create hoots blueprint

--- app.py
+++ app.py
@@ -6,13 +6,12 @@ import psycopg2, psycopg2.extras
 import bcrypt
 from auth_middleware import token_required
 from auth_blueprint import authentication_blueprint
+from hoots_blueprint import hoots_blueprint
 
 load_dotenv()
 
 app = Flask(__name__)
 app.register_blueprint(authentication_blueprint)
+app.register_blueprint(hoots_blueprint)
 
-
-
-# Run our application, by default on port 5000
 app.run()
