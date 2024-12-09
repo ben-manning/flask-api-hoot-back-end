deffa5a5947209ac20430ef434ceb05ef04a5dcc
Ben Manning <bmanning@me.com>
2024-12-09 14:02:57 -0500

    install CORS

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request, g
+from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 import jwt
@@ -12,6 +13,9 @@ from comments_blueprint import comments_blueprint
 load_dotenv()
 
 app = Flask(__name__)
+
+CORS(app)
+
 app.register_blueprint(authentication_blueprint)
 app.register_blueprint(hoots_blueprint)
 app.register_blueprint(comments_blueprint)
